c40a3941d76cac8afdec25143329eb679c6c2d2a
Karl Debiec <karl.t.debiec@gmail.com>
2013-07-22 08:12:33 -0400

    Fixed bug in primary_x.vmd.com related to in-progress trajectories

--- primary_x/vmd.py
+++ primary_x/vmd.py
@@ -2,14 +2,14 @@
 desc = """vmd.py
     Functions for primary cross-segment analysis of MD trajectories using Visual Molecular Dynamics
     Written by Karl Debiec on 13-07-18
-    Last updated 13-07-21"""
+    Last updated 13-07-22"""
 ########################################### MODULES, SETTINGS, AND DEFAULTS ############################################
 import os, sys, types
 import numpy as np
 from   standard_functions import shell_iterator
 ################################################## ANALYSIS FUNCTIONS ##################################################
 def com(segments, vmd = "vmd", selection = ["protein and name CA"], destination = "", **kwargs):
-    """ Calculates com of <selection> accounting for periodic boundary conditions using <vmd> """
+    """ Calculates com of <selection>(s) accounting for periodic boundary conditions using <vmd> """
     script          = "/".join(os.path.abspath(__file__).split("/")[:-2] + ["tcl", "vmd_com_x.tcl"])
     if isinstance(selection, types.StringType):
         selection   = [selection]
@@ -18,9 +18,9 @@ def com(segments, vmd = "vmd", selection = ["protein and name CA"], destination 
         destination = "_" + destination
 
     topology_string         = segments[0].topology
-    trajectory_string       = " ".join(["\"{0}\"".format(segment.trajectory)    for segment in segments])
-    selection_string        = " ".join(["\"{0}\"".format(sel.replace(" ", "_")) for sel     in selection])
-    selection_string_attr   = " ".join(["\"{0}\"".format(sel)                   for sel     in selection])
+    trajectory_string       = " ".join(["\"{0}\"".format(s.trajectory)        for s in segments if s.trajectory])
+    selection_string        = " ".join(["\"{0}\"".format(s.replace(" ", "_")) for s in selection])
+    selection_string_attr   = " ".join(["\"{0}\"".format(s)                   for s in selection])
     command                 = "{0} -dispdev text -e {1} -args -topology {2} -trajectory {3} -selection {4}".format(
                               vmd, script, topology_string, trajectory_string, selection_string)
     com                     = None
@@ -42,7 +42,7 @@ def _check_com(hdf5_file, segments, force = False, **kwargs):
     destination     = kwargs.get("destination", "")
     if not (destination == "" or destination.startswith("_")):
         destination = "_" + destination
-    expected        = [segment + "/com" + destination for segment in segments]
+    expected        = [s + "/com" + destination for s in segments]
     if (force
     or  not expected in hdf5_file): return [(com, kwargs)]
     else:                           return False
