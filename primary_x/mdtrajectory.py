8bcb81f49417f1a9f69c68038677e6d5bdaeb8aa
Marissa <mpp19@pitt.edu>
2013-10-21 14:06:18 -0400

    primary mdtrajectory to hdf5

--- primary_x/mdtrajectory.py
+++ primary_x/mdtrajectory.py
@@ -7,47 +7,15 @@ desc = """mdtrajectory.py
 import os, sys, json
 import numpy as np
 import mdtraj as md
+from standard_functions import topology_to_json
 ################################################## ANALYSIS FUNCTIONS ##################################################
 def topology(infile, **kwargs):
     """ Parses a pdb <infile> using MDTraj, and stores the resulting topology as a json string.
         Adapted from 'topology.py' in MDTraj """
     topology        = md.load(infile).topology
-    topology_dict   = {"chains":  [],
-                       "bonds":   []} 
+    topology_str    = topology_to_json(topology)
 
-    chain_iter      = topology.chains
-    if not hasattr(chain_iter, "__iter__"):
-        chain_iter  = chain_iter()
-    for chain in chain_iter:
-        chain_dict  = {"residues":  [], 
-                       "index":     int(chain.index)}
-
-        residue_iter        = chain.residues
-        if not hasattr(residue_iter, "__iter__"):
-            residue_iter    = residue_iter()
-        for residue in residue_iter:
-            residue_dict    = {"index": int(residue.index),
-                               "name":  str(residue.name),
-                               "atoms": []} 
-
-            atom_iter       = residue.atoms
-            if not hasattr(atom_iter, "__iter__"):
-                atom_iter   = atom_iter()
-            for atom in atom_iter:
-                residue_dict["atoms"].append({"index":   int(atom.index),
-                                              "name":    str(atom.name),
-                                              "element": str(atom.element.symbol)})
-            chain_dict["residues"].append(residue_dict)
-        topology_dict["chains"].append(chain_dict)
-
-    bond_iter       = topology.bonds
-    if not hasattr(bond_iter, "__iter__"):
-        bond_iter   = bond_iter()
-    for atom1, atom2 in bond_iter:
-        topology_dict["bonds"].append([int(atom1.index),
-                                       int(atom2.index)])
-
-    yield ("topology", {"json": str(json.dumps(topology_dict))})
+    yield ("topology", {"json": topology_str})
 
 def _check_topology(hdf5_file, force = False, **kwargs):
     if    (force
